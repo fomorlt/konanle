eb6189814262ba6bf687257302ce7b06454d4125
kokhle <kkhoadangle@gmail.com>
2024-09-06 19:41:07 -0700

    Update post-fetch.ts

--- src/lib/post-fetch.ts
+++ src/lib/post-fetch.ts
@@ -9,8 +9,15 @@ import matter from "gray-matter";
 //careful, this could be wrong
 const postsDirectory = path.join(process.cwd(), "content/blog");
 
+interface post {
+  title: string;
+  date: number;
+  tags: string[];
+}
+
 export function getSortedPostData() {
-  // new plan: function will return 2 things - list of posts, and parsed tags. 
+  //TODO: make this return the post interface
+  // new plan: function will return 2 things - list of posts, and parsed tags.
   // tags will be used for the type of post we want
   // list of posts will be used for the actual display of stuff
   const fileNames = fs.readdirSync(postsDirectory); //outputs an array of filename strings
@@ -20,28 +27,27 @@ export function getSortedPostData() {
     const fullPath = path.join("postsDirectory, fileName"); //creates filepath
     const fileContents = fs.readFileSync(fullPath, "utf8"); //grabs all data from markdown file
 
-    const matterResult = matter(fileContents); //returns an object with properties 'data', 'content', etc. 
+    const matterResult = matter(fileContents); //returns an object with properties 'data', 'content', etc.
     // alternatively, look at the documentation
     // https://www.npmjs.com/package/gray-matter
     // look at the '.read' section
     // that way, we wouldn't have to do all this .md stuff - maybe we could just start with the top stuff
     // wait, this script is still necessary to populate the list of posts... hm how do we do this?
-    // ok 
+    // ok
 
     //returns id (the filename), and the data from the markdown file (just the frontmatter, like title and tags (which will be an array))
     return {
       id,
-      ...matterResult.data
+      ...matterResult.data,
     };
   });
 
-  
-  //return posts by date 
-  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));  //can't do this, data doesn't necessarily have a date property. How do we ...
+  //return posts by date
+  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1)); //can't do this, data doesn't necessarily have a date property. How do we ...
 
   // TODO: figure out the return posts sorted by date object. Might need to utilize typescript here, to ensure that the type returned by the function has the date property.
   // interfaces! and stuff. At first I was thinking of seeing if there was some sort of type i could download from npm, but its just been a while since i've worked
-  // on this. I forgot that 'date' is part of the matter standard that I have set for myself. 
+  // on this. I forgot that 'date' is part of the matter standard that I have set for myself.
   // I want there to be a title, a date, ant an array of tags.
 }
 
